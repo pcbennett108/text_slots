b972119791ebeeff60b7cb3fc4b51e67904cf51c
pcbennett108 <pcbennett42@gmail.com>
2025-05-22 12:03:14 -0600

    get spin

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-MAX_LINES = 3
+import random
 
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
 
 def deposit():
     while True:
@@ -32,13 +51,40 @@ def get_number_of_lines():
     return lines
 
 
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} and ${MAX_BET}.")
+        else:
+            print("Please enter a number.")
+
+    return amount
+
+
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(
+                f"You do not have enough balance. Your balance is ${balance}.")
+        else:
+            break
+
+    print(
+        f"You are betting ${bet} on {lines} lines. Total bet is equal to ${bet * lines}.")
+
 
 
 main()
 
 
-# Tech With Tim time stopped at 12:45
+# Tech With Tim time stopped at 24:00
