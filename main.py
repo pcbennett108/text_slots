99543c84e15c0d63b266c79133af2c57ff39bcb5
pcbennett108 <pcbennett42@gmail.com>
2025-05-23 12:01:18 -0600

    build random symbols for a complete spin

--- main.py
+++ main.py
@@ -14,11 +14,26 @@ symbol_count = {
     "D": 8
 }
 
+
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
     for symbol, symbol_count in symbols.items():
         for _ in range(symbol_count):
             all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
 
 def deposit():
     while True:
@@ -87,4 +102,4 @@ def main():
 main()
 
 
-# Tech With Tim time stopped at 24:00
+#* Tech With Tim time stopped at 24:00
